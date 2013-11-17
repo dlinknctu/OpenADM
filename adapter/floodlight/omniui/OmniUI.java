e25e7a2c2627fc8ff0ef1b6b70720ec85e2a7206
hwchiu <sppsorrg@gmail.com>
2013-11-17 19:03:09 +0800

    Create the floodlight adapter module, it's a prototype.

---
+++ adapter/floodlight/omniui/OmniUI.java
@@ -0,0 +1,95 @@
+package net.floodlightcontroller.omniui;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.restserver.IRestApiService;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Set;
+import org.openflow.util.HexString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OmniUI implements IOmniUIService,IFloodlightModule  {
+	
+	protected IFloodlightProviderService floodlightProvider;
+	protected IRestApiService restApi;
+	protected static Logger logger;
+
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IOmniUIService.class);
+		return l;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		Map<Class<? extends IFloodlightService>, IFloodlightService> m = new HashMap<Class<? extends IFloodlightService>, IFloodlightService>();
+		m.put(IOmniUIService.class,this);
+		return m;
+	}
+
+	/*
+	 *
+	 * @brief: Tell the module loader we depend on
+	 */
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		// TODO Auto-generated method stub
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		l.add(IRestApiService.class);
+		return l;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+			floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+			restApi = context.getServiceImpl(IRestApiService.class);
+			logger = LoggerFactory.getLogger(OmniUI.class);
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) {
+		// TODO Auto-generated method stub
+		logger.info("QQQQQQQQQQQQQQQQQQ");
+		restApi.addRestletRoutable(new OmniUIWebRoutable());
+	}
+
+
+	/*  IOmniUIService implementation
+	 *  
+	 *
+	 */
+
+    public Set<SwitchInfo> getSwitches(){
+
+		return null;
+	}
+
+	public Set<LinkInfo> getLinks(){
+		return null;
+	}
+	
+	public Set<PortInfo> getPorts(){
+
+		return null;
+	}
+}
