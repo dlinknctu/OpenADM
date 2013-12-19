8eb3391894e9e8e16c574f7312fa8b487c573a78
Chia-Lin Cho <fox91119@gmail.com>
2013-12-19 21:20:13 +0800

    simple ADD REST API , with entry_name , without actions

---
+++ adapter/floodlight/omniui/SwitchAddMethod.java
@@ -0,0 +1,268 @@
+package net.floodlightcontroller.omniui;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.concurrent.ConcurrentHashMap;
+
+import net.floodlightcontroller.core.annotations.LogMessageCategory;
+import net.floodlightcontroller.core.annotations.LogMessageDoc;
+import net.floodlightcontroller.core.util.AppCookie;
+import net.floodlightcontroller.packet.IPv4;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFPacketOut;
+import org.openflow.protocol.OFPort;
+import org.openflow.protocol.action.OFAction;
+import org.openflow.protocol.action.OFActionDataLayerDestination;
+import org.openflow.protocol.action.OFActionDataLayerSource;
+import org.openflow.protocol.action.OFActionEnqueue;
+import org.openflow.protocol.action.OFActionNetworkLayerDestination;
+import org.openflow.protocol.action.OFActionNetworkLayerSource;
+import org.openflow.protocol.action.OFActionNetworkTypeOfService;
+import org.openflow.protocol.action.OFActionOutput;
+import org.openflow.protocol.action.OFActionStripVirtualLan;
+import org.openflow.protocol.action.OFActionTransportLayerDestination;
+import org.openflow.protocol.action.OFActionTransportLayerSource;
+import org.openflow.protocol.action.OFActionVirtualLanIdentifier;
+import org.openflow.protocol.action.OFActionVirtualLanPriorityCodePoint;
+import org.openflow.util.HexString;
+
+//1219
+import org.openflow.protocol.OFType;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import org.openflow.util.U16;
+import org.restlet.resource.ServerResource; //?
+//
+
+/**
+ * Represents static flow entries to be maintained by the controller on the 
+ * switches. 
+ */
+@LogMessageCategory("SwitchAddMethod")
+public class SwitchAddMethod {
+    protected static Logger log = LoggerFactory.getLogger(SwitchAddMethod.class);
+	public static IFloodlightProviderService floodlightProvider;	//1219
+	/*public static IFloodlightProviderService floodlightProvider =
+                (IFloodlightProviderService)getContext().getAttributes().
+                    get(IFloodlightProviderService.class.getCanonicalName());*/
+	
+	public static void setprovider(IFloodlightProviderService pro)
+	{
+		floodlightProvider = pro;
+	}
+	
+	public static Map<String, Object> jsonToStorageEntry(String fmJson) throws IOException {
+        Map<String, Object> entry = new HashMap<String, Object>();
+        MappingJsonFactory f = new MappingJsonFactory();
+        JsonParser jp;
+        
+        try {
+            jp = f.createJsonParser(fmJson);
+        } catch (JsonParseException e) {
+            throw new IOException(e);
+        }
+        
+        jp.nextToken();
+        if (jp.getCurrentToken() != JsonToken.START_OBJECT) {
+            throw new IOException("Expected START_OBJECT");
+        }
+        
+        while (jp.nextToken() != JsonToken.END_OBJECT) {
+            if (jp.getCurrentToken() != JsonToken.FIELD_NAME) {
+                throw new IOException("Expected FIELD_NAME");
+            }
+            
+            String n = jp.getCurrentName();
+            jp.nextToken();
+            if (jp.getText().equals("")) 
+                continue;
+            
+            if (n == "name")
+                entry.put(SwitchAddResource.COLUMN_NAME, jp.getText());
+            else if (n == "switch")
+                entry.put(SwitchAddResource.COLUMN_SWITCH, jp.getText());
+            else if (n == "actions")
+                entry.put(SwitchAddResource.COLUMN_ACTIONS, jp.getText());
+            else if (n == "priority")
+                entry.put(SwitchAddResource.COLUMN_PRIORITY, jp.getText());
+            else if (n == "active")
+                entry.put(SwitchAddResource.COLUMN_ACTIVE, jp.getText());
+            else if (n == "wildcards")
+                entry.put(SwitchAddResource.COLUMN_WILDCARD, jp.getText());
+            else if (n == "ingress-port")
+                entry.put(SwitchAddResource.COLUMN_IN_PORT, jp.getText());
+            else if (n == "src-mac")
+                entry.put(SwitchAddResource.COLUMN_DL_SRC, jp.getText());
+            else if (n == "dst-mac")
+                entry.put(SwitchAddResource.COLUMN_DL_DST, jp.getText());
+            else if (n == "vlan-id")
+                entry.put(SwitchAddResource.COLUMN_DL_VLAN, jp.getText());
+            else if (n == "vlan-priority")
+                entry.put(SwitchAddResource.COLUMN_DL_VLAN_PCP, jp.getText());
+            else if (n == "ether-type")
+                entry.put(SwitchAddResource.COLUMN_DL_TYPE, jp.getText());
+            else if (n == "tos-bits")
+                entry.put(SwitchAddResource.COLUMN_NW_TOS, jp.getText());
+            else if (n == "protocol")
+                entry.put(SwitchAddResource.COLUMN_NW_PROTO, jp.getText());
+            else if (n == "src-ip")
+                entry.put(SwitchAddResource.COLUMN_NW_SRC, jp.getText());
+            else if (n == "dst-ip")
+                entry.put(SwitchAddResource.COLUMN_NW_DST, jp.getText());
+            else if (n == "src-port")
+                entry.put(SwitchAddResource.COLUMN_TP_SRC, jp.getText());
+            else if (n == "dst-port")
+                entry.put(SwitchAddResource.COLUMN_TP_DST, jp.getText());
+        }
+        
+        return entry;
+    }
+	
+	//public static Map<String, Map<String, OFFlowMod>> parseRow(Map<String, Object> row, Map<String, Map<String, OFFlowMod>> entries) {
+	public static void parseRow(Map<String, Object> row, Map<String, Map<String, OFFlowMod>> entries) {
+        String switchName = null;
+        String entryName = null;
+
+        StringBuffer matchString = new StringBuffer();
+
+        OFFlowMod flowMod = (OFFlowMod) floodlightProvider.getOFMessageFactory()
+                .getMessage(OFType.FLOW_MOD);
+
+        if (!row.containsKey(SwitchAddResource.COLUMN_SWITCH) || !row.containsKey(SwitchAddResource.COLUMN_NAME)) {
+            log.debug(
+                    "skipping entry with missing required 'switch' or 'name' entry: {}",
+                    row);
+            //return entries;	//1219
+			return;
+        }
+        // most error checking done with ClassCastException
+        try {
+            // first, snag the required entries, for debugging info
+            switchName = (String) row.get(SwitchAddResource.COLUMN_SWITCH);
+            entryName = (String) row.get(SwitchAddResource.COLUMN_NAME);
+            if (!entries.containsKey(switchName))
+                entries.put(switchName, new HashMap<String, OFFlowMod>());
+            initDefaultFlowMod(flowMod, entryName);	//1219
+
+            for (String key : row.keySet()) {
+                if (row.get(key) == null)
+                    continue;
+                if (key.equals(SwitchAddResource.COLUMN_SWITCH) || key.equals(SwitchAddResource.COLUMN_NAME)
+                        || key.equals("id"))
+                    continue; // already handled
+                // explicitly ignore timeouts and wildcards
+                if (key.equals(SwitchAddResource.COLUMN_HARD_TIMEOUT) || key.equals(SwitchAddResource.COLUMN_IDLE_TIMEOUT) ||
+                        key.equals(SwitchAddResource.COLUMN_WILDCARD))
+                    continue;
+                if (key.equals(SwitchAddResource.COLUMN_ACTIVE)) {
+                    if  (!Boolean.valueOf((String) row.get(SwitchAddResource.COLUMN_ACTIVE))) {
+                        log.debug("skipping inactive entry {} for switch {}",
+                                entryName, switchName);
+                        entries.get(switchName).put(entryName, null);  // mark this an inactive
+                        //break;	//1219
+						return;
+                    }
+                } else if (key.equals(SwitchAddResource.COLUMN_ACTIONS)){
+                    //parseActionString(flowMod, (String) row.get(SwitchAddResource.COLUMN_ACTIONS), log);
+					continue;	//1219
+                } else if (key.equals(SwitchAddResource.COLUMN_COOKIE)) {
+                    flowMod.setCookie(
+                            computeEntryCookie(flowMod,
+                                    Integer.valueOf((String) row.get(SwitchAddResource.COLUMN_COOKIE)),
+                                    entryName));
+                } else if (key.equals(SwitchAddResource.COLUMN_PRIORITY)) {
+                    flowMod.setPriority(U16.t(Integer.valueOf((String) row.get(SwitchAddResource.COLUMN_PRIORITY))));
+                } else { // the rest of the keys are for OFMatch().fromString()
+                    if (matchString.length() > 0)
+                        matchString.append(",");
+                    matchString.append(key + "=" + row.get(key).toString());
+                }
+            }
+        } catch (ClassCastException e) {
+            if (entryName != null && switchName != null) {
+                log.warn(
+                        "Skipping entry {} on switch {} with bad data : "
+                                + e.getMessage(), entryName, switchName);
+            } else {
+                log.warn("Skipping entry with bad data: {} :: {} ",
+                        e.getMessage(), e.getStackTrace());
+            }
+        }
+
+        OFMatch ofMatch = new OFMatch();
+        String match = matchString.toString();
+        try {
+            ofMatch.fromString(match);
+        } catch (IllegalArgumentException e) {
+            log.debug(
+                    "ignoring flow entry {} on switch {} with illegal OFMatch() key: "
+                            + match, entryName, switchName);
+            //return entries;	//1219
+			return;
+        }
+        flowMod.setMatch(ofMatch);
+
+        entries.get(switchName).put(entryName, flowMod);
+		
+		//return entries;	//1219
+    }
+
+	public static void initDefaultFlowMod(OFFlowMod fm, String entryName) {
+        fm.setIdleTimeout((short) 0);   // infinite
+        fm.setHardTimeout((short) 0);   // infinite
+        fm.setBufferId(OFPacketOut.BUFFER_ID_NONE);
+        fm.setCommand((short) 0);
+        fm.setFlags((short) 0);
+        fm.setOutPort(OFPort.OFPP_NONE.getValue());
+        fm.setCookie(computeEntryCookie(fm, 0, entryName));  
+        fm.setPriority(Short.MAX_VALUE);
+    }	
+ 
+	public static long computeEntryCookie(OFFlowMod fm, int userCookie, String name) {
+        // flow-specific hash is next 20 bits LOOK! who knows if this 
+        /*
+        int prime = 211;
+        int flowHash = 2311;
+        for (int i=0; i < name.length(); i++)
+            flowHash = flowHash * prime + (int)name.charAt(i);
+        */
+        // For now we use 0 so we can do a mass delete by cookie
+        return AppCookie.makeCookie(10, 0);
+    }
+	
+	public static void writeFlowModToSwitch(long dpid, OFFlowMod flowMod) {
+        IOFSwitch ofSwitch = floodlightProvider.getSwitch(dpid);
+        if (ofSwitch == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Not deleting key {} :: switch {} not connected",
+                          dpid);
+            }
+            return;
+        }
+		//writeFlowModToSwitch(ofSwitch, flowMod);
+		
+        try {
+            ofSwitch.write(flowMod, null);
+            ofSwitch.flush();
+        } catch (IOException e) {
+            log.error("Tried to write OFFlowMod to {} but failed: {}",
+                    HexString.toHexString(ofSwitch.getId()), e.getMessage());
+        }
+    }
+}
---
+++ adapter/floodlight/omniui/SwitchAddResource.java
@@ -0,0 +1,219 @@
+package net.floodlightcontroller.omniui;
+
+import java.io.IOException;
+import java.util.Map;
+
+//1219
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.HAListenerTypeMarker;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IHAListener;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.IOFSwitchListener;
+import net.floodlightcontroller.core.ImmutablePort;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.util.AppCookie;
+import net.floodlightcontroller.restserver.IRestApiService;
+import net.floodlightcontroller.staticflowentry.web.StaticFlowEntryWebRoutable;
+import net.floodlightcontroller.storage.IResultSet;
+import net.floodlightcontroller.storage.IStorageSourceListener;
+import net.floodlightcontroller.storage.StorageException;
+
+import org.openflow.protocol.OFFlowMod;
+import org.openflow.protocol.OFFlowRemoved;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFType;
+import org.openflow.util.HexString;
+import org.openflow.util.U16;
+
+//
+
+import org.restlet.resource.Delete;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import net.floodlightcontroller.core.annotations.LogMessageCategory;
+import net.floodlightcontroller.core.annotations.LogMessageDoc;
+//import net.floodlightcontroller.staticflowentry.StaticFlowEntries;
+//import net.floodlightcontroller.staticflowentry.StaticFlowEntryPusher;
+import net.floodlightcontroller.storage.IStorageSourceService;
+
+/**
+ * Pushes a static flow entry to the storage source
+ * @author alexreimers
+ *
+ */
+@LogMessageCategory("SwitchAddResource")
+public class SwitchAddResource extends ServerResource {
+    protected static Logger log = LoggerFactory.getLogger(SwitchAddResource.class);
+
+	public static final String TABLE_NAME = "controller_switchaddtable";	//1219
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SWITCH = "switch_id";
+    public static final String COLUMN_ACTIVE = "active";
+    public static final String COLUMN_IDLE_TIMEOUT = "idle_timeout";
+    public static final String COLUMN_HARD_TIMEOUT = "hard_timeout";
+    public static final String COLUMN_PRIORITY = "priority";
+    public static final String COLUMN_COOKIE = "cookie";
+    public static final String COLUMN_WILDCARD = "wildcards";
+    public static final String COLUMN_IN_PORT = "in_port";
+    public static final String COLUMN_DL_SRC = "dl_src";
+    public static final String COLUMN_DL_DST = "dl_dst";
+    public static final String COLUMN_DL_VLAN = "dl_vlan";
+    public static final String COLUMN_DL_VLAN_PCP = "dl_vlan_pcp";
+    public static final String COLUMN_DL_TYPE = "dl_type";
+    public static final String COLUMN_NW_TOS = "nw_tos";
+    public static final String COLUMN_NW_PROTO = "nw_proto";
+    public static final String COLUMN_NW_SRC = "nw_src"; // includes CIDR-style
+                                                         // netmask, e.g.
+                                                         // "128.8.128.0/24"
+    public static final String COLUMN_NW_DST = "nw_dst";
+    public static final String COLUMN_TP_DST = "tp_dst";
+    public static final String COLUMN_TP_SRC = "tp_src";
+    public static final String COLUMN_ACTIONS = "actions";
+    public static String ColumnNames[] = { COLUMN_NAME, COLUMN_SWITCH,
+            COLUMN_ACTIVE, COLUMN_IDLE_TIMEOUT, COLUMN_HARD_TIMEOUT,
+            COLUMN_PRIORITY, COLUMN_COOKIE, COLUMN_WILDCARD, COLUMN_IN_PORT,
+            COLUMN_DL_SRC, COLUMN_DL_DST, COLUMN_DL_VLAN, COLUMN_DL_VLAN_PCP,
+            COLUMN_DL_TYPE, COLUMN_NW_TOS, COLUMN_NW_PROTO, COLUMN_NW_SRC,
+            COLUMN_NW_DST, COLUMN_TP_DST, COLUMN_TP_SRC, COLUMN_ACTIONS };
+	
+	
+    /**
+     * Checks to see if the user matches IP information without
+     * checking for the correct ether-type (2048).
+     * @param rows The Map that is a string representation of
+     * the static flow.
+     * @reutrn True if they checked the ether-type, false otherwise
+     */ 
+	 
+    private boolean checkMatchIp(Map<String, Object> rows) {
+        boolean matchEther = false;
+        String val = (String) rows.get(COLUMN_DL_TYPE);
+        if (val != null) {
+            int type = 0;
+            // check both hex and decimal
+            if (val.startsWith("0x")) {
+                type = Integer.parseInt(val.substring(2), 16);
+            } else {
+                try {
+                    type = Integer.parseInt(val);
+                } catch (NumberFormatException e) { /* fail silently */}
+            }
+            if (type == 2048) matchEther = true;
+        }
+
+        if ((rows.containsKey(COLUMN_NW_DST) ||
+                rows.containsKey(COLUMN_NW_SRC) ||
+                rows.containsKey(COLUMN_NW_PROTO) ||
+                rows.containsKey(COLUMN_NW_TOS)) &&
+                (matchEther == false))
+            return false;
+
+        return true;
+    }
+	
+	protected static Map<String, Map<String, OFFlowMod>> entriesFromStorage = new ConcurrentHashMap<String, Map<String, OFFlowMod>>();	///?
+	//Map<String, Map<String, OFFlowMod>> entries = new ConcurrentHashMap<String, Map<String, OFFlowMod>>(); //?
+	//HashMap<String, Map<String, OFFlowMod>> entriesToAdd = new HashMap<String, Map<String, OFFlowMod>>(); //?
+	
+    /**
+     * Takes a Static Flow Pusher string in JSON format and parses it into
+     * our database schema then pushes it to the database.
+     * @param fmJson The Static Flow Pusher entry in JSON format.
+     * @return A string status message
+     */
+    @Post
+    @LogMessageDoc(level="ERROR",
+        message="Error parsing push flow mod request: {request}",
+        explanation="An invalid request was sent to static flow pusher",
+        recommendation="Fix the format of the static flow mod request")
+    public String store(String fmJson) {
+        IStorageSourceService storageSource =
+                (IStorageSourceService)getContext().getAttributes().
+                    get(IStorageSourceService.class.getCanonicalName());
+					
+		IFloodlightProviderService floodlightProvider =
+                (IFloodlightProviderService)getContext().getAttributes().
+                    get(IFloodlightProviderService.class.getCanonicalName());
+		SwitchAddMethod.setprovider(floodlightProvider);
+		
+        Map<String, Object> rowValues;
+        try {
+            rowValues = SwitchAddMethod.jsonToStorageEntry(fmJson);
+            String status = null;
+            if (!checkMatchIp(rowValues)) {
+                status = "Warning! Pushing a static flow entry that matches IP " +
+                        "fields without matching for IP payload (ether-type 2048) will cause " +
+                        "the switch to wildcard higher level fields.";
+                log.error(status);
+            } else {
+                status = "Entry pushed";
+            }
+            storageSource.insertRowAsync(TABLE_NAME, rowValues);
+			
+			//1219
+			
+			//entriesFromStorage = SwitchAddMethod.parseRow(rowValues,entriesFromStorage);
+			SwitchAddMethod.parseRow(rowValues,entriesFromStorage);
+			for (String switchid : entriesFromStorage.keySet()) {
+				for (String entryname : entriesFromStorage.get(switchid).keySet())
+				{
+					if(entriesFromStorage.get(switchid).get(entryname) != null)
+					{
+						SwitchAddMethod.writeFlowModToSwitch(HexString.toLong(switchid),entriesFromStorage.get(switchid).get(entryname));
+					}
+				}
+			}
+			//
+			
+            return ("{\"status\" : \"" + status + "\"}");
+        } catch (IOException e) {
+            log.error("Error parsing push flow mod request: " + fmJson, e);
+            return "{\"status\" : \"Error! Could not parse flod mod, see log for details.\"}";
+        }
+    }
+/*
+    @Delete
+    @LogMessageDoc(level="ERROR",
+        message="Error deleting flow mod request: {request}",
+        explanation="An invalid delete request was sent to static flow pusher",
+        recommendation="Fix the format of the static flow mod request")
+    public String del(String fmJson) {
+        IStorageSourceService storageSource =
+                (IStorageSourceService)getContext().getAttributes().
+                    get(IStorageSourceService.class.getCanonicalName());
+        String fmName = null;
+        if (fmJson == null) {
+            return "{\"status\" : \"Error! No data posted.\"}";
+        }
+        try {
+            fmName = StaticFlowEntries.getEntryNameFromJson(fmJson);
+            if (fmName == null) {
+                return "{\"status\" : \"Error deleting entry, no name provided\"}";
+            }
+        } catch (IOException e) {
+            log.error("Error deleting flow mod request: " + fmJson, e);
+            return "{\"status\" : \"Error deleting entry, see log for details\"}";
+        }
+
+        storageSource.deleteRowAsync(StaticFlowEntryPusher.TABLE_NAME, fmName);
+        return "{\"status\" : \"Entry " + fmName + " deleted\"}";
+    }
+*/	
+}
