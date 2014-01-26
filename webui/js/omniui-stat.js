b16ad8c4b024fa5d3b5f18285c2a320d0cb4ac38
Min-Cheng Chan <rascov@gmail.com>
2014-01-26 17:26:58 +0800

    can fetch tsv from core. fix label of y-axis

--- webui/js/omniui-stat.js
+++ webui/js/omniui-stat.js
@@ -1,6 +1,9 @@
-var margin = {top: 20, right: 80, bottom: 30, left: 150},
+var margin = {top: 30, right: 50, bottom: 50, left: 150},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
+
+var yscale = 1000;
+var yscaletext = "KB/intvl.";
 
 var parseDate = d3.time.format("%Y-%m-%d %H:%M").parse;
 
@@ -22,74 +25,73 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
   .interpolate("basis")
-  .x(function(d) { return x(d.date); })
-  .y(function(d) { return y(d.temperature); });
+  .x(function(d) { return x(d.Time); })
+  .y(function(d) { return y(d.Byte/yscale); });
 
 var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+	  .attr("width", width + margin.left + margin.right)
+	  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("id", "transform")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	  .attr("id", "transform")
+	  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 function svgclear(){
 	$("#transform").empty();
 }
 
-function svgplot(){
-	d3.tsv("data.tsv", function(error, data) {
-	  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+function svgplot(resp){
+		data = d3.tsv.parse(resp);
+	  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "Time"; }));
 
 	  data.forEach(function(d) {
-	    d.date = parseDate(d.date);
+	    d.Time = parseDate(d.Time);
 	  });
 
 	  var cities = color.domain().map(function(name) {
 	    return {
 	      name: name,
 	      values: data.map(function(d) {
-	        return {date: d.date, temperature: +d[name]};
+	        return {Time: d.Time, Byte: +d[name]};
 	      })
 	    };
 	  });
 
-	  x.domain(d3.extent(data, function(d) { return d.date; }));
+	  x.domain(d3.extent(data, function(d) { return d.Time; }));
 
 	  y.domain([
-	    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-	    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+	    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.Byte/yscale; }); }),
+	    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.Byte/yscale; }); })
 	  ]);
 
 	  svg.append("g")
-	      .attr("class", "x axis")
-	      .attr("transform", "translate(0," + height + ")")
-	      .call(xAxis);
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
 	  svg.append("g")
 	      .attr("class", "y axis")
 	      .call(yAxis)
-	    .append("text")
+    	.append("text")
 	      .attr("transform", "rotate(-90)")
 	      .attr("y", 6)
 	      .attr("dy", ".71em")
 	      .style("text-anchor", "end")
-	      .text("Temperature (ºF)");
+	      .text("Traffic (" + yscaletext + ")");
 
 	  var city = svg.selectAll(".city")
-	      .data(cities)
+      .data(cities)
 	    .enter().append("g")
-	      .attr("class", "city");
+      .attr("class", "city");
 
 	  city.append("path")
-	      .attr("class", "line")
-	      .attr("d", function(d) { return line(d.values); })
-	      .style("stroke", function(d) { return color(d.name); });
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d) { return color(d.name); });
 
 	  city.append("text")
-	      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-	      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-	      .attr("x", 3)
-	      .attr("dy", ".35em")
-	      .text(function(d) { return d.name; });
-	});
+	    .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+	    .attr("transform", function(d) { return "translate(" + x(d.value.Time) + "," + y(d.value.Byte/yscale) + ")"; })
+	    .attr("x", 3)
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.name; });
 }
