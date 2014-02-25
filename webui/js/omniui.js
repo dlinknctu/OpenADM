16f8116422a5d4137f79b36d2e65ec14e8e7b2e3
starbops <starbops@gmail.com>
2014-02-25 14:15:20 +0800

    show the spinner while waiting, then reload the topo

--- webui/js/omniui.js
+++ webui/js/omniui.js
@@ -292,13 +292,37 @@ function loadJSONP(){
 }
 
 function sendFlow(f){
+    var opts = {
+        lines: 15, // The number of lines to draw
+        length: 9, // The length of each line
+        width: 3, // The line thickness
+        radius: 13, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+    var target = document.getElementById('waiting');
+    var spinner = new Spinner(opts);
+
     var url = "http://localhost:5567/flowmod";
     var data = JSON.stringify(f);
     var callback = function(data) {
         var stat = JSON.parse(data[2]);
         console.log(data);
-        alert(stat["status"]);
+        //alert(stat["status"]);
+        spinner.stop();
+        loadJSONP();
     };
+    spinner.spin(target);
     $.post(url, JSON.stringify(f), callback, "json");
 }
 
