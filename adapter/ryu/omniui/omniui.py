e00448fdcda7c0b4b365ad5be66f1e8c84209a40
stevenChang <steven19920206@gmail.com>
2014-12-25 05:24:44 +0800

    Flow Mod v1.3 Implemented

--- adapter/ryu/omniui/omniui.py
+++ adapter/ryu/omniui/omniui.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import ast
 from webob import Response
 from ryu.app.wsgi import ControllerBase, WSGIApplication, route
 from ryu.base import app_manager
@@ -14,6 +16,7 @@ from ryu.lib import ofctl_v1_0
 from ryu.lib import ofctl_v1_2
 from ryu.lib import ofctl_v1_3
 from ryu.lib.dpid import dpid_to_str
+from ryu.lib.dpid import str_to_dpid
 from ryu.topology.api import get_switch, get_link
 
 
@@ -32,6 +35,8 @@ class OmniUI(app_manager.RyuApp):
         self.data['waiters'] = self.waiters
         self.data['omniui'] = self
         mapper = wsgi.mapper
+        path = '/wm/omniui/'
+        format = 'json'
         wsgi.registory['RestController'] = self.data
 
         mapper.connect('omniui', '/wm/omniui/switch/json',
@@ -40,6 +45,10 @@ class OmniUI(app_manager.RyuApp):
         mapper.connect('omniui', '/wm/omniui/link/json',
                        controller=RestController, action='links',
                        conditions=dict(method=['GET']))
+        
+        mapper.connect('omniui', '/wm/omniui/add/json',
+                       controller=RestController, action='mod_flow_entry',
+                       conditions=dict(method=['POST']))
 
     @set_ev_cls([ofp_event.EventOFPFlowStatsReply,
                  ofp_event.EventOFPPortStatsReply,
@@ -143,7 +152,7 @@ class RestController(ControllerBase):
                         'dstIPMask': '-', # not support in ryu
                         'netProtocol': flow['match']['nw_proto'] if 'nw_proto' in flow['match'] else 0,
                         'srcIP': flow['match']['nw_src'] if 'nw_src' in flow['match'] else 0,
-			'srcIPMask': '-', # not support in ryu
+												'srcIPMask': '-', # not support in ryu
                         'dstPort': flow['match']['tp_dst'] if 'tp_dst' in flow['match'] else 0,
                         'srcPort': flow['match']['tp_src'] if 'tp_src' in flow['match'] else 0,
                         'vlan': flow['match']['dl_vlan'] if 'dl_vlan' in flow['match'] else 0,
@@ -205,6 +214,137 @@ class RestController(ControllerBase):
             result.append(omniLink) if reverse is False else None
         body = json.dumps(result)
         return Response(content_type='application/json', body=body)
+    
+
+    # repack flow mod information
+    def mod_flow_entry(self, req, **_kwargs):
+        try:
+            omniFlow = ast.literal_eval(req.body)
+        except SyntaxError:
+            LOG.debug('invalid syntax %s', req.body)
+            return Response(status=400)
+
+        switchID = omniFlow.get('switch')
+        dpid = switchID.replace(":",'')
+        dp = self.dpset.get(int(dpid))
+
+        if dp is None:
+            return Response(status=404)
+       
+        cmd = omniFlow.get('command')
+        if cmd == 'ADD':
+            cmd = dp.ofproto.OFPFC_ADD
+        elif cmd == 'MOD':
+            cmd = dp.ofproto.OFPFC_MODIFY
+        elif cmd == 'MOD_ST':
+            cmd = dp.ofproto.OFPFC_MODIFY_STRICT
+        elif cmd == 'DEL':
+            cmd = dp.ofproto.OFPFC_DELETE
+        elif cmd == 'DEL_ST':
+            cmd = dp.ofproto.OFPFC_DELETE_STRICT
+        else:
+            return Response(status=404)
+        
+        # repack Omniui Flow to Ryu Flow
+        
+        
+
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            ofctl_v1_0.mod_flow_entry(dp, ryuFlow, cmd)
+        elif dp.ofproto.OFP_VERSION == ofproto_v1_2.OFP_VERSION:
+            ofctl_v1_2.mod_flow_entry(dp, ryuFlow, cmd)
+        elif dp.ofproto.OFP_VERSION == ofproto_v1_3.OFP_VERSION:
+            ryuFlow = self.repack_flow_13(omniFlow, dp)
+            ofctl_v1_3.mod_flow_entry(dp, ryuFlow, cmd)
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+        '''
+        file = open('./log.txt', 'w')
+        test = "ryuFlow:%s\n" % ryuFlow
+        test += "dp: %s\n" % dp
+        test += "cmd:%s\n" % omniFlow.get('command')
+        file.write(test)
+        file.close()
+        '''
+        return Response(status=200)
+
+    def repack_flow_13(self, omniFlow, dp):
+        actions_type = omniFlow.get('actions').split('=')[0].upper()
+        actions_value = 0   
+        if actions_type == '':      #action_type = None, when not action is provided
+            actions_type = None
+        else:
+            actions_value = omniFlow.get('actions').split('=')[1]
+
+        ryuFlow = {
+            'cookie': int(omniFlow.get('cookie', 0)),
+            'cookie_mask': int(omniFlow.get('cookie_mask', 0)),
+            'table_id ': int(omniFlow.get('table_id', 0)),
+            'idle_timeout': int(omniFlow.get('idleTimeout', 0)),
+            'hard_timeout': int(omniFlow.get('hardTimeout', 0)),
+            'priority': int(omniFlow.get('priority', 0)),
+            #'buffer_id': int(omniFlow.get('buffer_id', dp.ofproto.OFP_NO_BUFFER)),
+            #'out_port': int(omniFlow.get('out_port', dp.ofproto.OFPP_ANY)),
+            #'out_group': int(omniFlow.get('out_group', dp.ofproto.OFPG_ANY)),
+            'flags': int(omniFlow.get('flags', 0)),
+            'match': {
+                'in_port': int(omniFlow.get('ingressPort', 0)),
+                #'in_phy_port': int(omniFlow.get('in_phy_port', 0)),
+                #'metadata': to_match_metadata,
+                'dl_dst': omniFlow.get('dstMac'),
+                'dl_src': omniFlow.get('srcMac'),
+                #'eth_dst': omniFlow.get('eth_dst'),
+                #'eth_src': omniFlow.get('eth_src'),
+                'dl_type': int(omniFlow.get('dlType', 0)),
+                #'eth_type': int(omniFlow.get('eth_type', 0)),
+                'dl_vlan': int(omniFlow.get('vlan', 0)),
+                #'vlan_vid': to_match_vid,
+                'vlan_pcp': int(omniFlow.get('vlanP', 0)),
+                #'ip_dscp': int(omniFlow.get('ip_dscp', 0)),
+                #'ip_ecn': int(omniFlow.get('ip_ecn', 0)),
+                'nw_proto': int(omniFlow.get('netProtocol', 0)),
+                #'ip_proto': int(omniFlow.get('ip_proto', 0)),
+                'nw_src': omniFlow.get('srcIP'),
+                'nw_dst': omniFlow.get('dstIP'),
+                #'ipv4_src': omniFlow.get('ipv4_src'),
+                #'ipv4_dst': omniFlow.get('ipv4_dst'),
+                ##'tp_src': int(omniFlow.get('srcPort', 0)),
+                ##'tp_dst': int(omniFlow.get('dstPort', 0)),
+                #'tcp_src': int(omniFlow.get('tcp_src', 0)),
+                #'tcp_dst': int(omniFlow.get('tcp_dst', 0)),
+                #'udp_src': int(omniFlow.get('udp_src', 0)),
+                #'udp_dst': int(omniFlow.get('udp_dst', 0)),
+                #'sctp_src': int(omniFlow.get('sctp_src', 0)),
+                #'sctp_dst': int(omniFlow.get('sctp_dst', 0)),
+                #'icmpv4_type': int(omniFlow.get('icmpv4_type', 0)),
+                #'icmpv4_code': int(omniFlow.get('icmpv4_code', 0)),
+                #'arp_op': int(omniFlow.get('arp_op', 0)),
+                #'arp_spa': to_match_ip,
+                #'arp_tpa': to_match_ip,
+                #'arp_sha': to_match_eth,
+                #'arp_tha': to_match_eth,
+                #'ipv6_src': to_match_ip,
+                #'ipv6_dst': to_match_ip,
+                #'ipv6_flabel': int(omniFlow.get('ipv6_flabel', 0)),
+                #'icmpv6_type': int(omniFlow.get('icmpv6_type', 0)),
+                #'icmpv6_code': int(omniFlow.get('icmpv6_code', 0)),
+                #'ipv6_nd_target': to_match_ip,
+                #'ipv6_nd_sll': to_match_eth,
+                #'ipv6_nd_tll': to_match_eth,
+                #'mpls_label': int(omniFlow.get('mpls_label', 0)),
+                #'mpls_tc': int(omniFlow.get('mpls_tc', 0)),
+                #'mpls_bos': int(omniFlow.get('mpls_bos', 0)),
+                #'pbb_isid': int(omniFlow.get('pbb_isid', 0)),
+                #'tunnel_id': int(omniFlow.get('tunnel_id', 0)),
+                #'ipv6_exthdr': int(omniFlow.get('ipv6_exthdr', 0))
+            },
+            'actions': [{
+                "type":actions_type,
+                "port":actions_value
+            }]
+        }
+        return ryuFlow
 
     # repack dpid
     def colonDPID(self, dpid):
