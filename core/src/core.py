76105488e5ba73b934718a6cb54c4827d748bf5f
Keats Hu <shhu@cs.nctu.edu.tw>
2015-06-16 22:38:14 +0800

    Add a new Feature - module depeance 	modified:   core.py

--- core/src/core.py
+++ core/src/core.py
@@ -82,6 +82,8 @@ class Core:
 		if config.has_key("LogFile"):
 			logFile = config['LogFile']
 		logging.basicConfig(filename = logFile, level = logLevel, format = '%(asctime)s - %(levelname)s: %(message)s')
+		console = logging.StreamHandler()
+		console.setLevel(logging.DEBUG)
 		#Modulize the controller adapter
 		if config.has_key("ControllerType"):
 			ControllerType = str(config['ControllerType']) + "_modules"
@@ -98,14 +100,44 @@ class Core:
 		plugins.__path__ = []
 		plugins.__path__.append (os.path.join(rootPath, 'src', ControllerType))
 
-		for module in config:
-			if module != "LogFile" and module != "REST" and module != "ControllerType" : # load modules other than LogFile and REST
-				instance = import_module('plugins.' + module.lower())
-				if(config.has_key(module)):
-					getattr(instance,module)(self,config[module])
-				else:
-					getattr(instance,module)(self,0)
+		moduleList = []
+		loadedModule = []
+		pastLoaded = []
 
+        # get module list
+		for module in config:
+			if module != "LogFile" and module != "REST" and module != "ControllerType" : # get modules other than LogFile and REST
+				moduleList.append(module)
+        # load modules
+		while set(moduleList) > set(loadedModule):
+			for module in moduleList:
+				if config[module].has_key('depend'):
+					depend_module = 0
+					tmp = config[module]['depend']
+					for depend in tmp:
+						if depend not in loadedModule:
+							depend_module = 1
+					if depend_module is 1:
+						continue
+				if module not in loadedModule:
+					logging.info("load {}!! ".format(module))
+					instance = import_module('plugins.' + module.lower())
+					if(config.has_key(module)):
+						getattr(instance,module)(self,config[module])
+					else:
+						getattr(instance,module)(self,0)
+					loadedModule.append(module)
+			if set(pastLoaded) == set(loadedModule):
+				errorModule = []
+				for module in moduleList:
+					if module not in loadedModule:
+						errorModule.append(module)
+				logging.critical("ERROR: Module Dependance")
+				logging.critical("The floowing modules has dependence problem:")
+				logging.critical(errorModule)
+				os.kill(os.getpid(),1)
+			pastLoaded = loadedModule[:]
+		logging.info("finish")
 		# Start REST service
 		if config.has_key("REST"):
 			restIP = config['REST']['ip']
@@ -246,5 +278,5 @@ def main():
 	coreInstance = Core()
 	coreInstance.start()
 	
-if __name__ ==  "__main__":
+if __name__ ==	"__main__":
 	main()
