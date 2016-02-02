8f732b9f6c54d409f32a991e2f42e3f63df97706
HsiaoHsuan <hsiaohsuan1l1l@gmail.com>
2016-02-02 07:31:00 -0800

    Revise busylink detection

--- core/src/floodlight_modules/busylink_detect.py
+++ core/src/floodlight_modules/busylink_detect.py
@@ -6,55 +6,69 @@ logger = logging.getLogger(__name__)
 class BusyLink_Detect:
 	def __init__(self,core,parm):
 		""" BusyLinkDetect init"""
-		self.controllerIP = "localhost"
-		self.controllerPort = "8080"
-		self.timerInterval = 5
-
 		self.coreIP = "localhost"
 		self.corePort = "5567"
 
 		self.baseState = 1
 		self.finalState = 3
-		self.threshold = 0.8
+		self.threshold = 0.000001
 		self.statistics = {}
 		self.capacity = {}
 		self.links = {}
-		self.switches= {}
+		self.switches = {}
 		self.BLD_result = []
 
-		#load config
-		if(parm):
-			if(parm.has_key("ip")):
-				self.controllerIP = parm["ip"]
-			if(parm.has_key("port")):
-				self.controllerPort = parm["port"]
-			if(parm.has_key("interval")):
-				self.timerInterval = int(parm["interval"])
-		logger.debug('IP =%s  port = %s  interval = %s' % (self.controllerIP,self.controllerPort,self.timerInterval))
-		core.registerEvent("periodicQuery",self.periodicQuery,self.timerInterval)
-		core.registerEventHandler("periodicQuery", self.busyLinkDetect)
-		core.registerSSEHandler("busylink", self.busylinkHandler)
+		core.registerEventHandler("linkbag", self.getLink)
+		core.registerEventHandler("portbag", self.getPort)
 
-	def busylinkHandler(self):
-		data = {}
-		for i in range(len(self.BLD_result)):
-			data[i] = self.BLD_result[i]
-		return json.dumps(data)
+	def getLink(self, linksbag):
+		#print '----------links----------'
+		#print json.dumps(linksbag.values())
 
-	def overthreshold(self,link,id):
-		link['state'] += 1
-		if link['state'] >= self.finalState:
-			link['state'] = self.finalState
+		try:
+			data = linksbag.values()
+			self.links = {}
+			for link in data:
+				tmp = {}
+				tmp['source'] = link[0]['dpid']
+				tmp['target'] = link[1]['dpid']
+				tmp['sourcePort'] = int(link[0]['port'])
+				tmp['targetPort'] = int(link[1]['port'])
+				id = "dpid %s, port %d -- dpid %s, port %d" % (tmp['source'], tmp['sourcePort'], tmp['target'], tmp['targetPort'])
+				self.links[id] = tmp
+		except Exception, e:
+			logger.error("json parse error for links: "+str(e))
+
+	def getPort(self, portsbag):
+		#print '----------ports----------'
+		#print json.dumps(portsbag.values())
+
+		try:
+			data = portsbag.values()
+			self.capacity = {}
+			self.switches = {}
+			for port in data:
+				self.capacity["%s_%d" % (port['dpid'],int(port['port']))] = self.parsePortFeatures(int(port['capacity']))
+				if port['dpid'] in self.switches:
+					self.switches[port['dpid']][int(port['port'])] = int(port['rxbyte'])
+				else:
+					self.switches[port['dpid']] = {}
+					self.switches[port['dpid']][int(port['port'])] = int(port['rxbyte'])
+		except Exception, e:
+			logger.error("json parse error for switch and features: "+str(e))
+
+		self.busyLinkDetect()
+
+	def overthreshold(self,id):
+		self.statistics[id]['state'] += 1
+		if self.statistics[id]['state'] >= self.finalState:
+			self.statistics[id]['state'] = self.finalState
 			self.BLD_result.append(id)
 	
-	def underthreshold(self,link):
-		link['state'] -= 1
-		if link['state'] < self.baseState:
-			link['state'] = self.baseState
-	
-	def periodicQuery(self):
-		self.periodicQueryLink()
-		self.periodicQueryPort()
+	def underthreshold(self,id):
+		self.statistics[id]['state'] -= 1
+		if self.statistics[id]['state'] < self.baseState:
+			self.statistics[id]['state'] = self.baseState
 	
 	def parsePortFeatures(self,features):
 		if features == 0:
@@ -73,102 +87,32 @@ class BusyLink_Detect:
 		if turn_binary[10] == '1' or turn_binary[11] == '1':
 			return 10*(1024**2)/8.0			#10Mb
 		return 0
-		
-	def queryLinkCapacity(self):
-		try:
-			conn = httplib.HTTPConnection(self.controllerIP, int(self.controllerPort))
-			conn.request("GET", "/wm/core/switch/all/features/json")
-			response = conn.getresponse().read()
-		except Exception, e:
-			logger.error("connection error for inquiring features: "+str(e))
-			return
-		finally:
-			conn.close()
-		try:
-			data = json.loads(response)
-			self.capacity = {}
-			for switch_id in data:
-				switch = data[switch_id]
-				ports = switch['ports']
-				for port in ports:
-					result = self.parsePortFeatures(port['currentFeatures'])
-					self.capacity["%s_%d" % (switch_id,port['portNumber'])] = result
-		except Exception, e:
-			logger.error("json parse error for features: "+str(e))
-		
-	def periodicQueryLink(self):
-		try:
-			conn = httplib.HTTPConnection(self.controllerIP, int(self.controllerPort))
-			conn.request("GET", "/wm/omniui/link/json")
-			response = conn.getresponse().read()
-		except Exception, e:
-			logger.error("connection error for inquiring links: "+str(e))
-			return
-		finally:
-			conn.close()
-		try:
-			data = json.loads(response)
-			self.links = {}
-			for link in data:
-				tmp = {}
-				tmp['source'] = link['src-switch']
-				tmp['target'] = link['dst-switch']
-				tmp['sourcePort'] = link['src-port']
-				tmp['targetPort'] = link['dst-port']
-				id = "dpid %s, port %s -- dpid %s, port %s" % (tmp['source'],tmp['sourcePort'],tmp['target'],tmp['targetPort'])
-				self.links[id] = tmp
-		except Exception, e:
-			logger.error("json parse error for links: "+str(e))
-		self.queryLinkCapacity()
-	
-	def periodicQueryPort(self):
-		try:
-			conn = httplib.HTTPConnection(self.controllerIP, int(self.controllerPort))
-			conn.request("GET", "/wm/omniui/switch/json")
-			response = conn.getresponse().read()
-		except Exception, e:
-			logger.error("connection error for inquiring switches: "+str(e))
-			return
-		finally:
-			conn.close()
-		try:
-			data = json.loads(response)
-			self.switches= {}
-			for switch in data:
-				tmp = {}
-				for port in switch['ports']:
-					tmp[int(port['port'])] = int(port['rxbyte'])
-				self.switches[switch['dpid']] = tmp
-		except Exception, e:
-			logger.error("json parse error for switch: "+str(e))
-	
+
 	def busyLinkDetect(self,event):
 		self.BLD_result = []
 		#calculate link's countBytes and capacity
 		for link_id in self.links:
-			link = self.links[link_id]
-			src = link['source']
-			srcp = link['sourcePort']
-			dest = link['target']
-			destp = link['targetPort']
+			src = self.links[link_id]['source']
+			srcp = self.links[link_id]['sourcePort']
+			dest = self.links[link_id]['target']
+			destp = self.links[link_id]['targetPort']
 			total_bytes = self.switches[src][srcp] + self.switches[dest][destp]
-			link['countBytes'] = total_bytes
-			link['capacity'] = min(self.capacity["%s_%d" % (src,srcp)],self.capacity["%s_%d" % (dest,destp)])
+			self.links[link_id]['countBytes'] = total_bytes
+			self.links[link_id]['capacity'] = min(self.capacity["%s_%d" % (src,srcp)],self.capacity["%s_%d" % (dest,destp)])
 		
 		#initialize self.statistics value
 		if len(self.statistics) == 0:
 			self.statistics = dict(self.links)
 			for link_id in self.statistics:
-				link = self.statistics[link_id]
-				link['state'] = self.baseState
+				self.statistics[link_id]['state'] = self.baseState
 		
 		#check threshold
 		for link_id in self.links:
 			if link_id in self.statistics:
 				if (self.links[link_id]['countBytes'] - self.statistics[link_id]['countBytes']) / self.statistics[link_id]['capacity'] >= self.threshold:
-					self.overthreshold(self.statistics[link_id],link_id)
+					self.overthreshold(link_id)
 				else:
-					self.underthreshold(self.statistics[link_id])
+					self.underthreshold(link_id)
 				self.statistics[link_id]['countBytes'] = self.links[link_id]['countBytes']
 			else:
 				self.statistics[link_id] = dict(self.links[link_id])
@@ -179,6 +123,12 @@ class BusyLink_Detect:
 				del self.statistics[link_id]
 		
 		#return result
-		if len(self.BLD_result)>0:
-			conn = httplib.HTTPConnection(self.coreIP,self.corePort)
-			conn.request('POST','/publish/busylink')
+		if len(self.BLD_result) > 0:
+			data = {}
+			for i in range(len(self.BLD_result)):
+				data[i] = self.BLD_result[i]
+
+			print '*****Busy Link ID*****'
+			print json.dumps(data)
+			#conn = httplib.HTTPConnection(self.coreIP,self.corePort)
+			#conn.request('POST','/publish/busylink')
