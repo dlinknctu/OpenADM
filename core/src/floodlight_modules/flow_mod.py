17837aceda7f9c428c2cc138cd1f0fd104253dfd
Chia-Lin Cho <fox91119@gmail.com>
2016-05-24 10:27:57 -0700

    Minor revise

--- core/src/floodlight_modules/flow_mod.py
+++ core/src/floodlight_modules/flow_mod.py
@@ -3,20 +3,21 @@ import json
 
 
 class Flow_mod:
-	def __init__(self,core,parm):		
+	def __init__(self,core,parm):
+		self.IP = parm["ip"]
+		self.Port = parm["port"]
 		self.path = "/wm/omniui/add/json"
 		self.headers = {
 			"Content-type": "application/json",
 			"Accept": "application/json",
             	}
 		# register rest api
-		self.Url="http://"+self.IP+":"+self.Port+"/wm/omniui/add/json"
 		core.registerRestApi("flowmod", self.flowHandler)
 
 	def flowHandler(self,data):
 		# return JSONP format
 		body = json.dumps(data.get_json(force=True))
-		conn = httplib.HTTPConnection(parm["ip"], parm["port"])
+		conn = httplib.HTTPConnection(self.IP, self.Port)
 		conn.request("POST", self.path, body, self.headers)
 		response = conn.getresponse()
 		ret = (response.status, response.reason, response.read())
