b72c6d2a1f3879db25367997f82b6165087be052
wnlee <wnlee@cs.nctu.edu.tw>
2016-09-10 14:47:13 +0800

    Refactor FlowtableSetting for performance

--- ui/src/js/components/Flowtable/FlowtableSetting.jsx
+++ ui/src/js/components/Flowtable/FlowtableSetting.jsx
@@ -2,28 +2,43 @@ import React, { Component, PropTypes } from 'react';
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
 import Checkbox from 'material-ui/Checkbox';
+import { onlyUpdateForKeys } from 'recompose';
 
 const SettingStyle = {
   style: { float: 'left', width: '20%', margin: 5 },
   iconStyle: { marginRight: 0 },
-}
+};
 
-const FlowtableSetting = ({ header, showColumn, showSetting, showColumnSetting, toggleSetting }) => (
+const SettingCheckbox = onlyUpdateForKeys(['field', 'showColumn'])(
+  ({ field, showColumn, showColumnSetting }) => <Checkbox
+    {...SettingStyle}
+    label={field}
+    checked={(showColumn.indexOf(field) !== -1)}
+    onCheck={(a, checked) => showColumnSetting({
+      value: field,
+      checked,
+    })}
+  />
+);
+
+const FlowtableSetting = ({
+  header,
+  showColumn,
+  showSetting,
+  showColumnSetting,
+  toggleSetting,
+}) => (
   <Dialog
     title="Setting the show column"
-    actions={[<RaisedButton label="Close" primary={true} onTouchTap={toggleSetting} />]}
+    actions={[<RaisedButton label="Close" primary={true} onClick={toggleSetting} />]}
     open={showSetting}
   >
     <div style={{ display: 'table' }}>
-      {header.map((d, i) => <Checkbox
-        {...SettingStyle}
+      {header.map((d, i) => <SettingCheckbox
         key={`ch-${i}`}
-        label={d}
-        checked={(showColumn.indexOf(d) != -1)}
-        onCheck={(a ,checked) => showColumnSetting({
-          value: d,
-          checked,
-        })}
+        field={d}
+        showColumn={showColumn}
+        showColumnSetting={showColumnSetting}
       />)}
     </div>
   </Dialog>
